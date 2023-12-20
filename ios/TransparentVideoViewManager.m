d9f4162e927202569835447454c3502d804752f2
emmyrlim <emmyrlim@gmail.com>
2023-12-19 16:03:46 -0800

    kill others

--- ios/TransparentVideoViewManager.m
+++ ios/TransparentVideoViewManager.m
@@ -3,8 +3,6 @@
 @interface RCT_EXTERN_MODULE(TransparentVideoViewManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(src, NSDictionary);
-RCT_EXPORT_VIEW_PROPERTY(loop, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(autoplay, BOOL);
 RCT_EXPORT_VIEW_PROPERTY(config, NSDictionary);
 
 @end
